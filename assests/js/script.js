a347590c5a51ac4c8b349d7db377c066b9f02436
Palomapc <palomaanajera@gmail.com>
2023-04-27 21:08:49 -0700

    prueba

---
+++ assests/js/script.js
@@ -0,0 +1,30 @@
+//function for dropdown menu
+function toggleDropdown() {
+    var dropdownContent = document.querySelector(".dropdown-content");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  }
+  
+  // redirect photos from the menu 
+  function openPage(url) {
+    window.location.href = url;
+  }
+  
+  //Change slide automatically
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 5 seconds
+  }
